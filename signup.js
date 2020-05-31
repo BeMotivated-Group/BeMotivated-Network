c269aee10be93f5a0a2d265f5ba666d1f6a52c43
Manoj Kumar <mkumariitdelhi@gmail.com>
2020-05-31 20:42:46 +0530

    kerberos id, post font, constraints

--- signup.js
+++ signup.js
@@ -1,12 +1,12 @@
 function postToGoogle() {
-                var entrynumber = $("#entrynumber").val();
+                var kerberos = $("#entrynumber").val();
                 var code = $("#code").val();
                 var name = $("#name").val();
                 var password = $("#password").val();
 
                 $.ajax({
                     url: "https://docs.google.com/forms/d/e/1FAIpQLScCsaYgEEEZvqmkZ2rNdOFaj_gLzi8Zz0_HptJpvC46KB3YJg/formResponse?",
-          data: {"entry.234205175": entrynumber, "entry.1140686131": code, "entry.569208933": name, "entry.907061108": password},
+          data: {"entry.234205175": kerberos, "entry.1140686131": code, "entry.569208933": name, "entry.907061108": password},
                     type: "POST",
                     dataType: "xml",
                     success: function(d)
