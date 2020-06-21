a65587357813b4520e65880a1c3cff68f24648ee
Dipen Kumar <dipenkumar@Dipens-MacBook-Air.local>
2020-06-21 13:23:32 +0545

    update

--- register.js
+++ register.js
@@ -1,20 +1,17 @@
-function postToGoogle() {
-                var kerberos = $("#emailid").val();
-                $.ajax({
-                    url: "https://docs.google.com/forms/d/e/1FAIpQLSfNaReT96NWmLYFfh_XFflym8b7B4WpPtN29j0d_1uloSMzBg/formResponse?",
-          data: {"entry.1955518625": kerberos},
-                    type: "POST",
-                    dataType: "xml",
-                    success: function(d)
-          {
-          },
-          error: function(x, y, z)
-            {
-
-              $('#form').hide();
-              alert("Mail has been sent. Soon you will get an otp on your webmail.");
-              window.open("signup.html", "_top");
-            }
-                });
-        return false;
-            }
+function postToGoogle(){
+    var kerberos=$("#emailid").val();
+    $.ajax({
+        url:"https://docs.google.com/forms/d/e/1FAIpQLSfNaReT96NWmLYFfh_XFflym8b7B4WpPtN29j0d_1uloSMzBg/formResponse?",
+        data:{
+            "entry.1955518625":kerberos
+        },
+        type:"POST",
+        dataType:"xml",
+        success:function(d){},
+        error:function(x,y,z){
+            alert("You will receive an email on "+kerberos.toLowerCase()+"@iitd.ac.in with the OTP.\nClick \"OK\" to continue with your IITD Email Verification.");
+            window.open("https://krdipen.github.io/BeMotivated/signup","_self");
+        }
+    });
+    return false;
+}
